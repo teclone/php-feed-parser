e3989eedcf34be555d13c851792241d686640d41
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-21 00:51:54 +0100

    feat: Create a FeedTypeNotSupported generic exception class

---
+++ src/Exceptions/FeedTypeNotSupportedException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\FeedParser\Exceptions;
+
+use Exception;
+
+
+class FeedTypeNotSupportedException extends Exception
+{
+    public function __construct(string $message, int $code = 0,
+        Exception $previous = null
+    ) {
+        Exception::__construct($message, $code, $previous);
+    }
+}
