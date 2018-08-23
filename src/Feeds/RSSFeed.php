11c5056c043f1ba01e856a5e8bdc315bc798e32b
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 21:29:03 +0100

    feat: create the RSS feed class, all rss feeds are instances of this class
    It provides the appropriate namespace map, alternate property xpath selector expressions that are
    used in parsing rss feeds

---
+++ src/Feeds/RSSFeed.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\FeedParser\Feeds;
+
+use Forensic\FeedParser\Enums\FeedTypes;
+use Forensic\FeedParser\XPath;
+use Forensic\FeedParser\FeedItems\RSSFeedItem;
+
+class RSSFeed extends BaseFeed
+{
+    public function __construct(XPath $xpath, string $default_lang,
+        bool $remove_styles, bool $remove_scripts)
+    {
+        $namespaces = [
+            'def' => 'http://purl.org/rss/1.0/',
+            'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+            'dc' => 'http://purl.org/dc/elements/1.1/',
+            'sy' => 'http://purl.org/rss/1.0/modules/syndication',
+            'enc' => 'http://purl.oclc.org/net/rss_2.0/enc#',
+            'content' => 'http://purl.org/rss/1.0/modules/content/',
+            'taxo' => 'http://purl.org/rss/1.0/modules/taxonomy/'
+        ];
+
+        $property_selectors = [
+            'id' => 'channel/title',
+            'title' => 'channel/title',
+            'link' => 'channel/link',
+            'description' => 'channel/description',
+            'image' => [
+                'src' => 'channel/image/url',
+                'link' => 'channel/image/link',
+                'title' => 'channel/image/title'
+            ],
+            'copyright' => 'channel/copyright',
+            'publisher' => 'channel/managingEditor || channel/webMaster',
+            'lastUpdated' => 'channel/pubDate',
+            'generator' => 'channel/generator',
+            'language' => 'channel/language',
+            'category' => 'channel/category'
+        ];
+
+        $items_selector = 'channel/item';
+
+        parent::__construct(
+            new FeedTypes(FeedTypes::RSS_FEED),
+            $default_lang,
+            $xpath,
+            $namespaces,
+            $property_selectors,
+            $items_selector,
+            $remove_styles,
+            $remove_scripts
+        );
+    }
+}
