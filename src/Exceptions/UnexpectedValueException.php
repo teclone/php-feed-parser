7170443340f968bec4e412e981fafe9cd3f8b86c
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 16:58:27 +0100

    feat: add an UnexpectedValue generic exception class

---
+++ src/Exceptions/UnexpectedValueException.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\FeedParser\Exceptions;
+use Exception;
+
+class UnexpectedValueException extends Exception
+{
+    public function __construct(string $message, int $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
