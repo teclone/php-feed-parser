79ed0747d0d0b6b3f54316375ebde1b876d7129c
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 21:26:03 +0100

    feat: create the ATOM feed class model, all atom feeds are instances of this class
    It provides the appropriate namespace mapping, property alternate xpath selector maps that are
    appropriate for parsing atom feeds

---
+++ src/Feeds/ATOMFeed.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\FeedParser\Feeds;
+
+use Forensic\FeedParser\Enums\FeedTypes;
+use Forensic\FeedParser\XPath;
+use Forensic\FeedParser\FeedItems\ATOMFeedItem;
+
+class ATOMFeed extends BaseFeed
+{
+    public function __construct(XPath $xpath, string $default_lang,
+        bool $remove_styles, bool $remove_scripts)
+    {
+        $namespaces = [
+            'atom' => 'http://www.w3.org/2005/Atom',
+            'xml' => 'http://www.w3.org/XML/1998/namespace'
+        ];
+
+        $property_selectors = [
+            'id' => 'atom:id',
+            'title' => 'atom:title', // text construct
+            'link' => 'atom:link[@rel="alternate"]/@href || atom:link/@href',
+            'description' => 'atom:subtitle', // text construct
+            'image' => [
+                'src' => 'atom:logo',
+                'link' => 'atom:link',
+                'title' => 'atom:title'
+            ],
+            'copyright' => 'atom:rights',
+            'publisher' => 'atom:contributor || atom:title',
+            'lastUpdated' => 'atom:updated',
+            'creator' => 'atom:generator',
+            'language' => '@xml:lang',
+            'category' => 'atom:category'
+        ];
+
+        $items_selector = 'atom:entry';
+
+        parent::__construct(
+            new FeedTypes(FeedTypes::ATOM_FEED),
+            $default_lang,
+            $xpath,
+            $namespaces,
+            $property_selectors,
+            $items_selector,
+            $remove_styles,
+            $remove_scripts
+        );
+    }
+}
