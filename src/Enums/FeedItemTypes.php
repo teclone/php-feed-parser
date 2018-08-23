e4142fccbb54340d2af51af07b19a7f74eaf1cd4
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 17:04:05 +0100

    refactor: inherit from base enum class rather than php non-standard splEnum extension class

--- src/Enums/FeedItemTypes.php
+++ src/Enums/FeedItemTypes.php
@@ -1,13 +1,17 @@
 <?php
 declare(strict_types = 1);
-
 namespace Forensic\FeedParser\Enums;
 
-class FeedItemTypes extends \SplEnum
+class FeedItemTypes extends BaseEnum
 {
-    const __default = self::RSS_FEED_ITEM;
+    const __default = 1;
 
     const RSS_FEED_ITEM = 1;
     const ATOM_FEED_ITEM = 2;
     const RDF_FEED_ITEM = 3;
+
+    public function __construct($value = null, bool $strict = true)
+    {
+        parent::__construct($value, $strict);
+    }
 }
--- src/Enums/FeedTypes.php
+++ src/Enums/FeedTypes.php
@@ -1,13 +1,17 @@
 <?php
 declare(strict_types = 1);
-
 namespace Forensic\FeedParser\Enums;
 
-class FeedTypes extends \SplEnum
+class FeedTypes extends BaseEnum
 {
-    const __default = self::RSS_FEED;
+    const __default = 1;
 
     const RSS_FEED = 1;
     const ATOM_FEED = 2;
     const RDF_FEED = 3;
+
+    public function __construct($value = null, bool $strict = true)
+    {
+        parent::__construct($value, $strict);
+    }
 }
