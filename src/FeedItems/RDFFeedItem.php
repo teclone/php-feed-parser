623191304fd534cf9ce23b094cc34174516f6881
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 21:23:18 +0100

    feat: create RDF feed item class, from which all rdf items are constructed
    The class provides the appropriated alternate xpath selector maps for rdf item properties

---
+++ src/FeedItems/RDFFeedItem.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\FeedParser\FeedItems;
+
+use Forensic\FeedParser\Enums\FeedItemTypes;
+use Forensic\FeedParser\XPath;
+use DOMElement;
+
+class RDFFeedItem extends BaseFeedItem
+{
+    public function __construct(DOMElement $item, XPath $xpath,
+        bool $remove_styles, bool $remove_scripts)
+    {
+        $property_selectors = [
+            'id' => '@rdf:about',
+            'title' => 'def:title || dc:title',
+            'link' => 'def:link',
+            'content' => 'content:encoded || dc:description || def:description',
+            'enclosure' => [
+                'type' => 'enc:enclosure/@enc:type',
+                'url' => 'enc:enclosure/@rdf:resource',
+                'length' => 'enc:enclosure/enc:length'
+            ],
+            'source' => 'dc:source',
+            //'image' => { 'src' => '', 'link' => '', 'title' => '' }, // to be parsed specially
+            'lastUpdated' => 'dc:date', // a date construct
+            'author' => 'dc:creator || dc:contributor',
+            'category' => 'dc:coverage || dc:subject/taxo:topic/@rdf:value || dc:subject || ' .
+                'parent::rdf:RDF/def:channel/dc:coverage || ' +
+            'parent::rdf:RDF/def:channel/dc:subject/taxo:topic/@rdf:value || ' .
+                'parent::rdf:RDF/def:channel/dc:subject' // defaults to the parent category
+        ];
+
+        parent::__construct(
+            new FeedItemTypes(FeedItemTypes::RDF_FEED_ITEM),
+            $item,
+            $xpath,
+            $property_selectors,
+            $remove_styles,
+            $remove_scripts
+        );
+    }
+}
