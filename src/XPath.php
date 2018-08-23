192ce38b467904563cfe686406550c7bf432d8af
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-23 12:33:06 +0100

    refactor: update method return types, and integrate context node resolver

--- src/XPath.php
+++ src/XPath.php
@@ -10,40 +10,65 @@ use DOMNode;
 class XPath
 {
     private $_dom_xpath = null;
+    private $_context_node = null;
 
     public function __construct(DOMDocument $doc)
     {
         $this->_dom_xpath = new DOMXPath($doc);
     }
 
+    public function getContextNode()
+    {
+        return $this->_context_node;
+    }
+
+    public function setContextNode(DOMNode $context_node)
+    {
+        $this->_context_node = $context_node;
+    }
+
+    protected function resolveContextNode(DOMNode $context_node = null)
+    {
+        if (!is_null($context_node))
+            return $context_node;
+        else
+            return $this->_context_node;
+    }
+
     /**
      * registers namespace
      *
+     *@param string $prefix - the namespace prefix
+     *@param string $namespace_prefix - the namespace uri
      *@return boolean
     */
-    public function registerNamespace(string $prefix, string $namespacePrefix)
+    public function registerNamespace(string $prefix, string $namespace_prefix)
     {
-        return $this->_dom_xpath->registerNamespace($prefix, $namespacePrefix);
+        return $this->_dom_xpath->registerNamespace($prefix, $namespace_prefix);
     }
 
     /**
      * registers namespaces
+     *
+     *@param array $namespaces - array of namespace prefix=>uri key value pairs
     */
     public function registerNamespaces(array $namespaces)
     {
-        foreach($namespaces as $prefix => $namespacePrefix)
-            if (is_string($prefix) && is_string($namespacePrefix))
-                $this->registerNamespace($prefix, $namespacePrefix);
+        foreach($namespaces as $prefix => $namespace_prefix)
+            if (is_string($prefix) && is_string($namespace_prefix))
+                $this->registerNamespace($prefix, $namespace_prefix);
     }
 
     /**
      * queries the document and returns a single result item or null
      *
+     *@param string $expressions - the xpath selector expression
+     *@param DOMNode $context_node - the selector context
      *@return DOMNode|null
     */
     public function selectNode(string $expression, DOMNode $context_node = null)
     {
-        $result = $this->_dom_xpath->query($expression, $context_node);
+        $result = $this->_dom_xpath->query($expression, $this->resolveContextNode($context_node));
         if ($result === false || $result->length === 0)
             return null;
 
@@ -51,14 +76,17 @@ class XPath
     }
 
     /**
-     * queries the document and returns a a DOMNodeList or null
+     * queries the document and returns a DOMNodeList of results or null
+     * if no result is found
      *
+     *@param string $expressions - the xpath selector expression
+     *@param DOMNode $context_node - the selector context
      *@return DOMNodeList|null
     */
     public function selectNodes(string $expression, DOMNode $context_node = null)
     {
-        $result = $this->_dom_xpath->query($expression, $context_node);
-        if ($result === false)
+        $result = $this->_dom_xpath->query($expression, $this->resolveContextNode($context_node));
+        if ($result === false || $result->length === 0)
             return null;
 
         return $result;
@@ -67,6 +95,8 @@ class XPath
     /**
      * queries the document and returns a single result item or null
      *
+     *@param string $expressions - the alternate xpath selector expressions
+     *@param DOMNode $context_node - the selector context
      *@return DOMNode|null
     */
     public function selectAltNode(string $expressions, DOMNode $context_node = null)
@@ -81,8 +111,11 @@ class XPath
     }
 
     /**
-     * queries the document and returns a DOMNodeList or null
+     * queries the document and returns a DOMNodeList or null using alternate xpath
+     * selector xpressions
      *
+     *@param string $expressions - the alternate xpath selector expressions
+     *@param DOMNode $context_node - the selector context
      *@return DOMNodeList|null
     */
     public function selectAltNodes(string $expressions, DOMNode $context_node = null)
